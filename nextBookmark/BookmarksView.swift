219dc9f741a910f69caad1af7b3d8917cc047c3a
Kai <git@altepizza.de>
2020-04-11 03:37:21 +0200

    #12 Overhauled folder navigation

--- nextBookmark/BookmarksView.swift
+++ nextBookmark/BookmarksView.swift
@@ -12,68 +12,114 @@ import SwiftUIRefresh
 import NotificationBannerSwift
 
 struct BookmarksView: View {
+    //@State private var currentFolder = -1
     @State private var isShowing = false
     @State private var searchText : String = ""
-    @State var folders: [Folder] = [
-        .init(id: -20, title: "<Pull down to load your bookmarks>",  parent_folder_id: -10, books: [])
-    ]
+    private let defaultFolder: Folder = .init(id: -20, title: "<Pull down to load your bookmarks>",  parent_folder_id: -10, books: [])
+    @State var folders: [Folder] = [.init(id: -20, title: "<Pull down to load your bookmarks>",  parent_folder_id: -10, books: [])]
+    
+    @State var currentRoot : Folder = Folder(id: -1, title: "/", parent_folder_id: -1, books: [])
+    
+    struct OpenFolderRow: View {
+        var folder: Folder
+        var body: some View {
+            HStack(){
+                Image(systemName: "folder")
+                Text(folder.title).fontWeight(.bold)
+            }
+        }
+    }
+    
+    struct FolderRow: View {
+        var folder: Folder
+        var body: some View {
+            HStack(){
+                Image(systemName: "folder.fill")
+                Text(folder.title).fontWeight(.bold)
+            }
+        }
+    }
+    
+    struct BackFolderRow: View {
+        var body: some View {
+            HStack(){
+                Image(systemName: "arrowshape.turn.up.left")
+            }
+        }
+    }
     
     var body: some View {
         NavigationView{
             VStack{
                 SearchBar(text: $searchText, placeholder: "Filter bookmarks")
+                OpenFolderRow(folder: self.currentRoot)
+                
                 List {
-                    ForEach(self.folders) { folder in
-                        FolderRow(folder: folder)
-                        ForEach(folder.books.filter {
-                            self.searchText.isEmpty ? true : $0.title.lowercased().contains(self.searchText.lowercased()) || $0.url.lowercased().contains(self.searchText.lowercased())
-                        }) { book in
-                            BookmarkRow(book: book)
+                    if self.currentRoot.id > -1 {
+                        BackFolderRow().onTapGesture {
+                            
+                            self.currentRoot = self.folders.first(where: {$0.id == self.currentRoot.parent_folder_id})!
+                            
+                            CallNextcloud().get_all_bookmarks_for_folder(folder: self.currentRoot) { bookmarks in
+                                if let bookmarks = bookmarks {
+                                    self.currentRoot.books = bookmarks
+                                    self.isShowing = false
+                                }
+                            }
                         }
-                        .onDelete(perform: { row in
-                            self.delete(folder: folder, row: row)
-                        })
-                        
-                        
                     }
+
+                    
+                    ForEach(self.folders.filter {
+                        $0.parent_folder_id == self.currentRoot.id && $0.id != self.currentRoot.id
+                    }) { folder in
+                        FolderRow(folder: folder).onTapGesture {
+                            self.currentRoot = folder
+                            CallNextcloud().get_all_bookmarks_for_folder(folder: self.currentRoot) { bookmarks in
+                                if let bookmarks = bookmarks {
+                                    self.currentRoot.books = bookmarks
+                                    self.isShowing = false
+                                }
+                            }
+                        }}
+                    ForEach(currentRoot.books.filter {
+                        self.searchText.isEmpty ? true : $0.title.lowercased().contains(self.searchText.lowercased()) || $0.url.lowercased().contains(self.searchText.lowercased())
+                    }) { book in
+                        BookmarkRow(book: book)
+                    }
+                    .onDelete(perform: { row in
+                        self.delete(folder: self.currentRoot, row: row)
+                    })
+                    
                 }
             }
             .pullToRefresh(isShowing: $isShowing) {
                 self.startUpCheck()
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    CallNextcloud().requestFolderHierarchy() { jason in
-                        if let jason = jason {
-                            self.folders =  CallNextcloud().makeFolders(json: jason)
-                            self.folders.append(Folder(id: -1, title: "/", parent_folder_id: -1, books: []))
-                            for i in self.folders.indices {
-                                let fff = self.folders[i] as Folder
-                                CallNextcloud().get_all_bookmarks_for_folder(folder: fff) { bookmarks in
-                                    if let bookmarks = bookmarks {
-                                        self.folders[i].books = bookmarks
-                                        self.isShowing = false
-                                    }
+                            CallNextcloud().get_all_bookmarks_for_folder(folder: self.currentRoot) { bookmarks in
+                                if let bookmarks = bookmarks {
+                                    self.currentRoot.books = bookmarks
+                                    self.isShowing = false
                                 }
                             }
-                        }
-                    }
                 }
             }.navigationBarTitle("Bookmarks", displayMode: .inline)
                 .navigationBarItems(trailing: NavigationLink(destination: SettingsView()) {
                     Text("Settings")})
         }.navigationViewStyle(StackNavigationViewStyle())
             .onAppear() {
+                self.startUpCheck()
                 CallNextcloud().requestFolderHierarchy() { jason in
                     if let jason = jason {
                         self.folders =  CallNextcloud().makeFolders(json: jason)
                         self.folders.append(Folder(id: -1, title: "/", parent_folder_id: -1, books: []))
-                        for i in self.folders.indices {
-                            let fff = self.folders[i] as Folder
-                            CallNextcloud().get_all_bookmarks_for_folder(folder: fff) { bookmarks in
-                                if let bookmarks = bookmarks {
-                                    self.folders[i].books = bookmarks
-                                    self.isShowing = false
-                                }
+                        self.currentRoot = Folder(id: -1, title: "/", parent_folder_id: -1, books: [])
+                        CallNextcloud().get_all_bookmarks_for_folder(folder: self.currentRoot) { bookmarks in
+                            if let bookmarks = bookmarks {
+                                self.currentRoot.books = bookmarks
+                                self.isShowing = false
                             }
+                            
                         }
                     }
                 }
@@ -96,12 +142,8 @@ struct BookmarksView: View {
     
     func delete(folder: Folder, row: IndexSet) {
         for index in row {
-            for i in self.folders.indices {
-                if folders[i].id == folder.id {
-                    CallNextcloud().delete(bookId: folders[i].books[index].id)
-                    folders[i].books.remove(at: index)
-                }
-            }
+            CallNextcloud().delete(bookId: folder.books[index].id)
+            self.currentRoot.books.remove(at: index)
         }
     }
 }
@@ -132,18 +174,7 @@ struct BookmarkRow: View {
     }
 }
 
-struct FolderRow: View {
-    var folder: Folder
-    var body: some View {
-        HStack(){
-            Image(systemName: "folder")
-            Text(folder.title).fontWeight(.bold)
-            //                .onTapGesture {
-            //                    self.folder.isExpanded = false
-            //            }
-        }
-    }
-}
+
 
 private func tagsAvailable(for book: Bookmark) -> Bool {
     if (book.tags.isEmpty) {
@@ -176,6 +207,18 @@ struct SearchBar: UIViewRepresentable {
         func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             text = searchText
         }
+        
+        func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+            text = ""
+            searchBar.text = ""
+            searchBar.resignFirstResponder()
+            searchBar.endEditing(true)
+        }
+        
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.resignFirstResponder()
+            searchBar.endEditing(true)
+        }
     }
     
     func makeCoordinator() -> SearchBar.Coordinator {
@@ -184,10 +227,12 @@ struct SearchBar: UIViewRepresentable {
     
     func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
         let searchBar = UISearchBar(frame: .zero)
+        //searchBar.delegate = context.coordinator
         searchBar.delegate = context.coordinator
         searchBar.placeholder = placeholder
         searchBar.searchBarStyle = .minimal
         searchBar.autocapitalizationType = .none
+        searchBar.showsCancelButton = true
         return searchBar
     }
     
