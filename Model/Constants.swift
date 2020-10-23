9a4afbe0d6e48af52492c4183a68315adc8e2854
Kai <git@altepizza.de>
2020-10-23 16:52:23 +0200

    Simplified About Page

---
+++ Model/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Credits.swift
+//  nextBookmark
+//
+//  Created by Kai Rieger on 23.10.20.
+//  Copyright © 2020 Kai Rieger. All rights reserved.
+//
+
+import Foundation
+
+let CREDITS = [
+    "Alamofire": "https://github.com/Alamofire/Alamofire",
+    "JGProgressHUD": "https://github.com/JonasGessner/JGProgressHUD",
+    "KeychainSwift": "https://github.com/evgenyneu/keychain-swift",
+    "Nextcloud": "https://nextcloud.com/",
+    "Nextcloud Bookmarks": "https://github.com/nextcloud/bookmarks",
+    "NotificationBanner": "https://github.com/Daltron/NotificationBanner",
+    "SwiftUI-Refresh": "https://github.com/siteline/SwiftUIRefresh",
+    "SwiftyJSON": "https://github.com/SwiftyJSON/SwiftyJSON"
+]
--- View/ThanksView.swift
+++ View/ThanksView.swift
@@ -14,102 +14,41 @@ extension UIApplication {
     }
 }
 
-struct ThanksView: View {
-    @State private var show_modal: Bool = false
+struct LinkButton: View {
+    let urlString: String
+    let header: String
+    
     var body: some View {
-        NavigationView{
-            VStack{
-                Text("Feedback, Issues, Features, Code?...")
-                Button(action: {
-                    guard let url = URL(string: "https://gitlab.com/altepizza/nextbookmark") else { return }
-                    UIApplication.shared.open(url)
-                }) {
-                    Text("Visit me!")
-                }
-                
-                Spacer()
-                
-                Button(action: {
-                    guard let url = URL(string: "https://gitlab.com/altepizza/nextbookmark/-/raw/master/privacy_policy.md") else { return }
-                    UIApplication.shared.open(url)
-                }) {
-                    Text("Privacy Policy")
-                }
-                
-                Spacer()
-                
-                VStack {
-                    Text("Also thanks to...")
-                    
-                    Button(action: {
-                        guard let url = URL(string: "https://github.com/Alamofire/Alamofire") else { return }
-                        UIApplication.shared.open(url)
-                    }) {
-                        Text("Alamofire")
-                    }
-                    
-                    Button(action: {
-                        let url = URL(string: "https://github.com/JonasGessner/JGProgressHUD")
-                        UIApplication.shared.open(url!)
-                    }) {
-                        Text("JGProgressHUD")
-                    }
-                    
-                    Button(action: {
-                        let url = URL(string: "https://github.com/evgenyneu/keychain-swift")
-                        UIApplication.shared.open(url!)
-                    }) {
-                        Text("KeychainSwift")
-                    }
-                    
-                    Button(action: {
-                        guard let url = URL(string: "https://nextcloud.com/") else { return }
-                        UIApplication.shared.open(url)
-                    }) {
-                        Text("Nextcloud")
-                    }
-                    
-                    Button(action: {
-                        guard let url = URL(string: "https://github.com/nextcloud/bookmarks") else { return }
-                        UIApplication.shared.open(url)
-                    }) {
-                        Text("Nextcloud Bookmarks")
-                    }
-                    
-                    Button(action: {
-                        guard let url = URL(string: "https://github.com/Daltron/NotificationBanner") else { return }
-                        UIApplication.shared.open(url)
-                    }) {
-                        Text("NotificationBanner")
-                    }
-                    
-                    Button(action: {
-                        guard let url = URL(string: "https://github.com/siteline/SwiftUIRefresh") else { return }
-                        UIApplication.shared.open(url)
-                    }) {
-                        Text("SwiftUI-Refresh")
-                    }
-                    
-                    Button(action: {
-                        guard let url = URL(string: "https://github.com/SwiftyJSON/SwiftyJSON") else { return }
-                        UIApplication.shared.open(url)
-                    }) {
-                        Text("SwiftyJSON")
-                    }
-                }.padding()
-                
-                Spacer()
-                
-                VStack {
-                    Text(UIApplication.appVersion ?? "X.X.X")
-                }.padding()
-            }
-        }.navigationBarTitle("About", displayMode: .inline)
-        .navigationViewStyle(StackNavigationViewStyle())
+        Button(action: {
+            guard let url = URL(string: urlString) else { return }
+            UIApplication.shared.open(url)
+        }) {
+            Text(header)
+        }
     }
 }
 
-
+struct ThanksView: View {
+    var body: some View {
+        Form {
+            Section (header: Text("Feedback, Issues, Features, Code?...")) {
+                LinkButton(urlString: "https://gitlab.com/altepizza/nextbookmark", header: "Visit me!")
+            }
+            Section (header: Text("Thanks")) {
+                ForEach(CREDITS.sorted(by: <), id: \.key) { key, value in
+                    LinkButton(urlString: value, header: key)
+                }
+            }
+            Section {
+                LinkButton(urlString: "https://gitlab.com/altepizza/nextbookmark/-/raw/master/privacy_policy.md", header: "Privacy Policy")
+            }
+            Section (header: Text("Version")) {
+                Text(UIApplication.appVersion ?? "X.X.X")
+            }
+        }
+        .navigationBarTitle("About", displayMode: .inline)
+    }
+}
 
 struct ThanksView_Previews: PreviewProvider {
     static var previews: some View {
